063bc6efd5afd088342206fe407dae04fab85065
Edward Castillo <castilloedwarda@gmail.com>
2019-10-25 14:24:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,8 +51,8 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do
-    
+    return false if source_array[i] == FALSE
     i += 1
   end
-  
+  return true
 end
